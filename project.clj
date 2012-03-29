2b370f73471a59003561f64378f1ef2fe0bd5fd6
Mark Grant <merely.semantics@gmail.com>
2012-03-29 10:02:31 -0500

    Final version before clojars

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject com.scriptling/clojureserver "1.0.0-SNAPSHOT"
+(defproject com.scriptling/clojureserver "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :plugins [[lein-swank "1.4.4"]]
   :javac-options {:destdir "classes/"}
-  :java-source-path "src/server/java"
-  :main server.main)
+  :java-source-path "src/server/java")
--- src/server/app.clj
+++ src/server/app.clj
@@ -1,7 +1,17 @@
 (ns server.app
   (:use [server.core :only [config]]
-        [server.ageents :only [generate-agents send-socket-to-http-agents]])
+        [server.agents :only [generate-agents send-socket-to-http-agents]])
   (:import java.net.ServerSocket))
+
+
+(def server-agent (agent false))
+
+(declare run-server)
+(defn app-start [router & options]
+  (let [options-hash (apply assoc options)]
+    (if (:async options)
+      (send-off server-agent run-server router)
+      (run-server router))))
 
 (defn run-server [router]
   (let [port (Integer/parseInt (config "port"))
@@ -9,7 +19,7 @@
     (println (str "Server starting on port " port "..."))
     (loop []
       (let [client (.accept server)
-            agents (generate-agents integrated-router)]
+            agents (generate-agents router)]
         (.setSoTimeout client 1000)
         (send-socket-to-http-agents agents client)
         (recur)))))
